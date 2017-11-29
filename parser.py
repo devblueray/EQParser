cdf4ef09a68f83a36630957417cd5875d587a982
Ubuntu <ubuntu@instance-2.c.hsd-work.internal>
2017-11-29 09:54:13 +0000

    Data structure now how i want it
    ToDo:
    Add to database
    Display on web

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 import re
 import json
 import sqlite3
+from collections import defaultdict
 from flask import Flask, request,render_template
 
 app=Flask(__name__)
@@ -14,57 +15,24 @@ items = {}
 banks = []
 files = ['iltar-bank', 'boop-bank']
 
-def insertItems(file):
-    with open(file, 'r') as file:
-      for line in file:
-        if not re.match(r'^\s*$', line):
-          line = line.strip('\n').split('\t')
-          if line[1] != "Empty" and line[0].startswith("Bank"):
-            if items.get(line[1]) == None:
-              print("Value: " + 1*line[3])
-              items[line[1]] = [(1*int(line[3])),[]]
-              print(type(line[3]))
-            else:
-              items[line[1]][0] += int(line[3])
-            if file.name.partition("-")[0] not in items[line[1]][1]:
-                    items[line[1]][1].append(file.name.partition("-")[0])
-
-
 def newInsertItems(*args):
-    for file in args:
-        print(file)
-        with open(file, 'r') as current:
-            for line in current:
-                if not re.match(r'^\s*$', line):
-                    line = line.strip('\n').split('\t')
-                    if line[1] != "Empty" and line[0].startswith("Bank"):
-                        if items.get(line[1]) == None:
-                            items[line[1]] = [{"Hi":2}] #must have this for now, it inits the list/dict(will try to fix later)
-                            items[line[1]][0]=({current.name:line[3]})
-                        else:
-                            items[line[1]][0].update({current.name:line[3]})
-    print(items)
+  for file in args:
+    with open(file, 'r') as current:
+      inventory = [line.strip('\n').split('\t') for line in current]
+      for item in inventory:
+        if (re.match('Bank[0-9]-', item[0])):
+          if (not re.match('Empty', item[1])):
+            if item[1] not in items:
+              items[item[1]] = {file: item[3]}
+            else:
+              items[item[1]].update({file: item[3]})
+    return items
     
-    #print("TEST", items['Backpack'])                           
-newInsertItems(*files)
-#for item in items:
-#    data = []
-#    data.append(item)
-#    data.append(items[item][0])
-#    strTmp = ""
- #   for fname in items[item][1]:
- #       strTmp += fname + " "
- #   data.append(strTmp)
-    
-    #print("{}: Count: {} File: {}".format(item, items[item][0], strTmp))
-#    c.execute('''insert into skybank(key,value,character) values(?,?,?);''',(data))
-conn.commit()
-
-print("DATABASE")
-a = c.execute("select * from skybank;").fetchall()
+inventory = newInsertItems(*files)
+print(inventory)
 @app.route('/')
 def index():
     return render_template('item_list.html', items=a)
 
-if __name__ == "__main__":
-    app.run()
+#if __name__ == "__main__":
+#    app.run()
